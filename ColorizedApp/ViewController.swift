19a34f0814afb2422b2fd495c4dca3e3ee21f85a
WiNkY51 <110772668+WiNkY51@users.noreply.github.com>
2024-07-15 05:44:37 +0300

    Result homework

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -7,12 +7,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    @IBOutlet var redColorLabel: UILabel!
+    @IBOutlet var greenColorLabel: UILabel!
+    @IBOutlet var blueColorLabel: UILabel!
+    
+    private var redColorIndex: CGFloat = 1
+    private var greenColorIndex: CGFloat = 1
+    private var blueColorIndex: CGFloat = 1
+
+    @IBAction func setRedColor(_ sender: UISlider!) {
+        redColorLabel.text = sender.value.formatted()
+        redColorIndex = CGFloat(sender.value)
+        setColorView()
     }
-
-
+    
+    @IBAction func setGreenColor(_ sender: UISlider!) {
+        greenColorLabel.text = sender.value.formatted()
+        greenColorIndex = CGFloat(sender.value)
+        setColorView()
+    }
+    
+    @IBAction func setBlueColor(_ sender: UISlider!) {
+        blueColorLabel.text = sender.value.formatted()
+        blueColorIndex = CGFloat(sender.value)
+        setColorView()
+    }
+    
+    private func setColorView() {
+        view.backgroundColor = .init(red: redColorIndex, green: greenColorIndex, blue: blueColorIndex, alpha: 1)
+    }
+    
 }
+
 
